4c152878e1129c1ffbd1f693b8452bd969a54d5f
praththi <praththisathiyan@gmail.com>
2024-06-11 10:47:53 +0530

    partially completed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
 import job from "./cron/cron.js";
-
+import cors from "cors";
 dotenv.config();
 
 connectDB();
@@ -21,6 +21,7 @@ const __dirname = path.resolve();
 
 
 // Middlewares
+app.use(cors());
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
